b8493c6b4446603b6476dae44017a6a97a5c744d
NuzhatUlAin <nuzhatulain.28@gmail.com>
2025-08-17 18:31:37 +0500

    Initial commit

---
+++ JobTrackerApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using JobTrackerApi.Models;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+    public DbSet<JobApplication> JobApplications { get; set; }
+}
---
+++ JobTrackerApi/Models/JobApplication.cs
@@ -0,0 +1,13 @@
+﻿namespace JobTrackerApi.Models
+{
+    public class JobApplication
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+        public string Role { get; set; }
+        public string Status { get; set; } // Applied, Interview, Offer, Rejected
+        public DateTime DateApplied { get; set; }
+        public string? Notes { get; set; }
+        public string? ResumeLink { get; set; }
+    }
+}
